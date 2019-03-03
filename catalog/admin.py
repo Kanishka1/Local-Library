21896d23ac5730a94b70ab8e859426bd632bbd57
Kanishka1 <37743397+Kanishka1@users.noreply.github.com>
2019-03-03 10:11:12 +0530

    Add files via upload

---
+++ catalog/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+from catalog.models import Author, Genre, Book, BookInstance
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(Language)
+#admin.site.register(BookInstance)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
